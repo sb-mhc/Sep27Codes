b125ec36a3ce143bafeeed1eacaefcc800f3ba24
sb-mhc <sbiswal@mtholyoke.edu>
2021-09-27 07:12:08 -0400

    GUI starter code

---
+++ src/FirstGUI.java
@@ -0,0 +1,15 @@
+import javafx.application.Application;
+import javafx.stage.*;
+
+public class FirstGUI extends Application {
+
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage pStage) {
+		pStage.show();
+	}
+}
