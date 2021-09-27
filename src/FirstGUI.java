8e810bc346a6f0b13e4142275f176425dbb84f37
sb-mhc <sbiswal@mtholyoke.edu>
2021-09-27 16:57:39 -0400

    Added the FirstGUI

--- src/FirstGUI.java
+++ src/FirstGUI.java
@@ -1,4 +1,8 @@
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.*;
 
 public class FirstGUI extends Application {
@@ -8,10 +12,40 @@ public class FirstGUI extends Application {
 		launch(args);
 	}
 	
+	
 	@Override
 	public void start(Stage primaryStage) {
 		
+		//Contents of the application here!
+		BorderPane bpane = new BorderPane();
+		Button b = new Button("Click me");
+		bpane.setCenter(b);
+		
+		b.setOnMouseClicked(this::respondToClick); // this calls ehandeler on click
+		
+		//Scene
+		Scene s1 = new Scene(bpane,800,500);// Will do it in 5 minutes
+		
+		//Add scene to stage
+		primaryStage.setScene(s1);
+		
+		//Show stage
+		
+		primaryStage.setTitle("Click application");
+		primaryStage.sizeToScene(); // size of the scene instead of explicitly specify H and W
 		primaryStage.show();
 		
 	}
+	
+	
+	public void respondToClick(MouseEvent e) {
+		
+		Button evtSrc = (Button)e.getSource(); // type-cast the source of event to button 
+		
+		if(evtSrc.getText() == "Click me")
+			evtSrc.setText("Clicked");
+		else
+			evtSrc.setText("Click me");
+	}
+	
 }
