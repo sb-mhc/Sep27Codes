5b0cecd1169d7c131ef6213f8132e336263ed240
sb-mhc <sbiswal@mtholyoke.edu>
2021-09-27 07:21:20 -0400

    primaryStage

--- src/FirstGUI.java
+++ src/FirstGUI.java
@@ -9,7 +9,7 @@ public class FirstGUI extends Application {
 	}
 	
 	@Override
-	public void start(Stage pStage) {
-		pStage.show();
+	public void start(Stage primaryStage) {
+		primaryStage.show();
 	}
 }
